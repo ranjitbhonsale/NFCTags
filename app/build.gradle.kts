d342f5915d419f04a2c7c3c9eeca65e259d67e4c
Ranjit Bhonsale <ranjit.bhonsale@gmail.com>
2026-09-02 20:33:00 +0530

    Increment version code to 2 for Google Play

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,9 @@ android {
     defaultConfig {
         applicationId = "work.ranjit.nfctags"
         minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
