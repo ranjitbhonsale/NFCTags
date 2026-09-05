6f6ea9305b39e8c16a8ecb97f2e6fd2eeb209f7b
Ranjit Bhonsale <ranjit.bhonsale@gmail.com>
2026-09-05 22:45:31 +0530

    Migrate from kapt to KSP for Room compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.compose.compiler)
   alias(libs.plugins.kotlin.serialization)
-  id("kotlin-kapt")
+  alias(libs.plugins.ksp)
 }
 
 android {
@@ -92,5 +92,5 @@ dependencies {
   val room_version = "2.6.1"
   implementation("androidx.room:room-runtime:$room_version")
   implementation("androidx.room:room-ktx:$room_version")
-  kapt("androidx.room:room-compiler:$room_version")
+  ksp("androidx.room:room-compiler:$room_version")
 }
