ac64bfba1db0c498bdb427d5f38837798cf1478d
Ranjit Bhonsale <ranjit.bhonsale@gmail.com>
2026-09-06 19:30:36 +0530

    Bump versionCode to 6 for Google Play upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "work.ranjit.nfctags"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
 
     buildTypes {
